c47c27fe57820e468490a6b23995a641a57b0396
hilljtm <47020214+hilljtm@users.noreply.github.com>
2020-02-23 14:17:35 -0500

    added table, and display, need to fix SSL error

--- table.py
+++ table.py
@@ -3,7 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 
-df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+df = pd.read_csv('CSV/BTC-USD.csv')
 
 
 def generate_table(dataframe, max_rows=10):
@@ -23,7 +23,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(children=[
-    html.H4(children='US Agriculture Exports (2011)'),
+    html.H4(children='Bitcoin historical prices(2020)'),
     generate_table(df)
 ])
 
